d5c7aabcba8732c01ce6375b151e60253b1e6e8e
kaiyamag <kmagnuson24@wooster.edu>
2024-07-23 19:51:08 -0700

    Add python template

---
+++ py_template.py
@@ -0,0 +1,20 @@
+"""
+Solving https://open.kattis.com/problems/
+
+Kaiya Magnuson, 7/23/2024
+
+Strategy:
+
+"""
+
+def main():
+    # Parse one line of space-separated input
+    my_input = input().split(" ")
+
+    # Parse a file in read mode
+    with open("text.txt", "r") as file:
+        for line in file:
+            pass
+
+if __name__=="__main__":
+    main()
